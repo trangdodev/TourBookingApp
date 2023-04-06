42c995721a94dd02ffea04a54c78e23c16042974
Trang Do <trangdo.contact@gmail.com>
2023-04-06 11:00:18 +0700

    profile fragment

--- app/src/main/java/com/project/travelbooking/ProfileFragment.java
+++ app/src/main/java/com/project/travelbooking/ProfileFragment.java
@@ -4,9 +4,15 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.support.annotation.Nullable;
+import android.text.Editable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+
+import java.io.Serializable;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,12 +23,12 @@ public class ProfileFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String ARG_PARAM1 = "loggedUserData";
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private Map user;
+
+    EditText edUName, edUPhone;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -33,15 +39,13 @@ public class ProfileFragment extends Fragment {
      * this fragment using the provided parameters.
      *
      * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment ProfileFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
+    public static ProfileFragment newInstance(Map param1) {
         ProfileFragment fragment = new ProfileFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putSerializable(ARG_PARAM1, (Serializable) param1);
         fragment.setArguments(args);
         return fragment;
     }
@@ -50,8 +54,7 @@ public class ProfileFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            user = (Map) getArguments().getSerializable(ARG_PARAM1);
         }
     }
 
@@ -60,5 +63,13 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        edUName = getView().findViewById(R.id.edUName);
+        edUPhone = getView().findViewById(R.id.edUPhone);
+        edUName.setText(user.get("name").toString());
+        edUPhone.setText(user.get("phone").toString());
     }
 }
